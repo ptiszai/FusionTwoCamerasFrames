b9d4a9b2f45f3df58e7ca460255286156c6ac022
ptiszai <tiszaii@hotmail.com>
2016-11-04 11:21:33 +0100

    Version 1.0

--- source/stdafx.cpp
+++ source/stdafx.cpp
@@ -1,7 +1,7 @@
 /*******************************************************************************#
-#           FusionTwoCamerasFrames												#
-#           Istvan Tiszai <tiszaii@hotmail.com>                                 #
-#			https://github.com/ptiszai/FusionTwoCamerasFrames					#
+#	FusionTwoCamerasFrames							#
+#	Istvan Tiszai <tiszaii@hotmail.com>                                 	#
+#	https://github.com/ptiszai/FusionTwoCamerasFrames			#
 #                                                                               #
 # This program is free software; you can redistribute it and/or modify          #
 # it under the terms of the GNU General Public License as published by          #
